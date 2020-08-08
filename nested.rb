e51d0f9786162add92fbdf0cfe22822d65950a86
Will Gonzalez <will.colocho@gmail.com>
2020-08-08 07:15:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash.unshift(:yukihiro_matsumoto => {
+programmer_hash.[:yukihiro_matsumoto => {
    :known_for => "Ruby",
-   :languages => ["LISP", "C"]})
+   :languages => ["LISP", "C"]}]
    programmer_hash
 end
 
